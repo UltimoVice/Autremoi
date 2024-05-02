9dea544fadc02f2a48edeba37fd39e9558ecd2e8
SilviaIannini <ianninisilvia@gmail.com>
2024-05-02 12:23:45 +0200

    Scroll

---
+++ src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export function Cards(title,name,price){
+    return(
+        <div>
+            <img>{images}</img>
+            <h4>{title}</h4>
+            <p>{price}</p>
+        </div>
+    )
+        
+
+    
+}
