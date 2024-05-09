eed37ffcba85691109be41ca818bb115a68e9fd0
Marcello Sciulara <marcello.sciulara2014@gmail.com>
2024-05-09 17:56:29 +0200

    a

--- src/components/admin/DataTable.jsx
+++ src/components/admin/DataTable.jsx
@@ -2,18 +2,16 @@ import { Link } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import React, { useState } from "react";
 
-
 const DataTable = (props) => {
- 
-  
   const handleDelete = (id, e) => {
     //delete item
     console.log(id + "has been deleted!");
-props.setProducts((products) => products.filter((deleted) => deleted.id !== id))
+    props.setProducts((products) =>
+      products.filter((deleted) => deleted.id !== id)
+    );
     console.log(props.products);
-   if (e) e.stopPropagation();
+    if (e) e.stopPropagation();
   };
- 
 
   const actionColumns = {
     field: "action",
@@ -21,19 +19,19 @@ props.setProducts((products) => products.filter((deleted) => deleted.id !== id))
     width: 200,
     renderCell: (params) => {
       return (
-        <div className="flex items-center text-center justify-center p-0 h-full">        
-          <div className="flex items-center text-center justify-center p-0"> 
-          <Link to={`/${props.slug}/${params.row.id}`}>
-            <button
-              className="flex items-center text-center justify-center"
-              onClick={() => handleDelete(params.row.id)}
-            >
-              <img
-                className="w-5 h-5"
-                src="src/assets/Admin/trash.svg"
-                alt=""
-              />
-            </button>
+        <div className="flex items-center text-center justify-center p-0 h-full">
+          <div className="flex items-center text-center justify-center p-0">
+            <Link to={`/${props.slug}/${params.row.id}`}>
+              <button
+                className="flex items-center text-center justify-center"
+                onClick={() => handleDelete(params.row.id)}
+              >
+                <img
+                  className="w-5 h-5"
+                  src="src/assets/Admin/trash.svg"
+                  alt=""
+                />
+              </button>
             </Link>
           </div>
         </div>
@@ -42,10 +40,10 @@ props.setProducts((products) => products.filter((deleted) => deleted.id !== id))
   };
 
   return (
-    <div className="bg-white w-full max-w-full" >
+    <div className="bg-white w-full max-w-full">
       <DataGrid
-        className="p-5 flex items-center text-center justify-center"
-        rows={props.rows}
+        className="p-5 flex items-center text-center justify-center w-full"
+        rows={props.products}
         columns={[...props.columns, actionColumns]}
         initialState={{
           pagination: {
